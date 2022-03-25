97d4d2d47cca1969daee8fa6dd820b12b6737306
Tuannt <tuannt1531@gmail.com>
2022-03-25 10:56:16 +0700

    refactor bot

--- src/trading_bot.py
+++ src/trading_bot.py
@@ -3,7 +3,8 @@ from unittest import result
 from configuration import *
 from binance_api import *
 import time
-
+import sys
+import os
 
 class TradingBot():
     def __init__(self, bot_info = BotInfo()):
@@ -17,39 +18,49 @@ class TradingBot():
         self.binance_api = BinaceAPI(self.bot_info.binance_secret_key, self.bot_info.binance_api_key, self.bot_info.Testnet_url)
 
     def check_buy(self, price):
-        quantity_per_buy = 0
-        if(self.lowest_price == 0):
-            self.lowest_price = price
-        if(price < self.lowest_price):
-            self.lowest_price = price
-            self.is_track_buy = True
-        if(self.is_track_buy == True):
-            if(price > self.lowest_price):
-                delta_condition = ((price - self.lowest_price) / self.lowest_price) * 100
-                print("========================DEBUG - CHECK_BUY: delta = {}".format(delta_condition))
-                if(delta_condition >= self.bot_info.delta_buy):
-                    quantity_per_buy = self.bot_info.quantity_per_buy
-                    self.is_track_buy = False
-                    self.avg_price = (self.avg_price + price) / 2
+        try:
+            quantity_per_buy = 0
+            if(self.lowest_price == 0):
+                self.lowest_price = price
+            if(float(price) < float(self.lowest_price)):
+                self.lowest_price = price
+                self.is_track_buy = True
+            if(self.is_track_buy == True):
+                if(float(price) > float(self.lowest_price)):
+                    delta_condition = ((price - self.lowest_price) / self.lowest_price) * 100
+                    print("========================DEBUG - CHECK_BUY: delta = {}".format(delta_condition))
+                    if(float(delta_condition) >= float(self.bot_info.delta_buy)):
+                        quantity_per_buy = float(self.bot_info.quantity_per_buy)
+                        self.is_track_buy = False
+                        self.avg_price = (self.avg_price + price) / 2
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
         return quantity_per_buy
     
     def check_sell(self, price):
-        quantity_per_sell = 0
-        if(price > self.eth_price):
-            self.eth_price = price
-        if(self.avg_price == 0):
-            self.avg_price = price
-        if(price > self.avg_price):
-            delta_condition = ((price - self.avg_price) / self.avg_price) * 100
-            print("========================DEBUG - CHECK_SELL: delta = {}".format(delta_condition))
-            if delta_condition >= self.bot_info.profit + self.bot_info.delta_sell:
-                self.is_track_sell = True
-            if self.is_track_sell == True:
-                if(price < self.eth_price):
-                    delta_condition = ((self.eth_price - price) / price) * 100
-                    if(delta_condition >= self.bot_info.delta_sell):
-                        quantity_per_sell = self.bot_info.quantity_per_sell
-                        self.is_track_sell = False
+        try:
+            quantity_per_sell = 0
+            if(float(price) > float(self.eth_price)):
+                self.eth_price = price
+            if(self.avg_price == 0):
+                self.avg_price = price
+            if(float(price) > float(self.avg_price)):
+                delta_condition = ((price - self.avg_price) / self.avg_price) * 100
+                print("========================DEBUG - CHECK_SELL: delta = {}".format(delta_condition))
+                if float(delta_condition) >= float(self.bot_info.profit) + float(self.bot_info.delta_sell):
+                    self.is_track_sell = True
+                if self.is_track_sell == True:
+                    if(float(price) < float(self.eth_price)):
+                        delta_condition = ((self.eth_price - price) / price) * 100
+                        if(float(delta_condition) >= float(self.bot_info.delta_sell)):
+                            quantity_per_sell = float(self.bot_info.quantity_per_sell)
+                            self.is_track_sell = False
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
         
         self.pre_price = price
         return quantity_per_sell
@@ -58,7 +69,7 @@ class TradingBot():
     def run(self):
         checking_cnt = 10
         while True:
-            time.sleep(1)
+            time.sleep(5)
             URL = URLConfiguration('./config/bot_config.ini')
             getURL = URL.load_url()
             symbol = self.bot_info.symbol
@@ -67,13 +78,16 @@ class TradingBot():
                 price = value['price']
                 print("* symbol = {} - price = {}".format(symbol, price))
                 # check buy
-                if(self.check_buy(float(price))):
+                quality = self.check_buy(float(price))
+                if(quality > 0):
                     print("* symbol = {} - price = {} ============================================= action = BUY =================================".format(symbol, price))
-                    print(self.binance_api.trade_order_market(getURL.test_oder, symbol, "BUY", 0.001))
-                if(self.check_sell(float(price))):
+                    print(self.binance_api.trade_order_market(getURL.test_oder, symbol, "BUY", quality))
+                quality = self.check_sell(float(price))
+                if(quality > 0):
                     print("* symbol = {} - price = {} ============================================= action = SELL ================================".format(symbol, price))
-                    print(self.binance_api.trade_order_market(getURL.test_oder, symbol, "SELL", 0.001))
-                checking_cnt += 1
+                    self.lowest_price = price
+                    print(self.binance_api.trade_order_market(getURL.test_oder, symbol, "SELL", quality))
+                checking_cnt += 2
                 if(checking_cnt >= 10):
                     checking_cnt = 0
                     balances = self.binance_api.account_infor(getURL.acc_infor)["balances"]
@@ -85,6 +99,9 @@ class TradingBot():
                         if(balance["asset"] == "BTC"):
                             BTC_total = balance["free"]
                     print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ USDT_total = {} - BTC_total = {} =================================".format(USDT_total, BTC_total))
-            except:
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
                 pass
             pass
